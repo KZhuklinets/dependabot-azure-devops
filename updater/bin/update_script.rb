5c6d19592b900b54d824deb15d52c0c686727bcb
kzhuklinets <53328474+kzhuklinets@users.noreply.github.com>
2024-07-31 11:29:06 +0100

    Update update_script.rb

--- updater/bin/update_script.rb
+++ updater/bin/update_script.rb
@@ -511,12 +511,16 @@ end
 
 puts "source '#{$source}'"
 puts "creds '#{$options[:credentials]}'"
+puts "1"
 azure_client1 = TingleSoftware::Dependabot::Clients::Azure.for_source(
   source: $source,
   credentials: $options[:credentials]
 )
+puts "2"
 user_id = azure_client1.get_user_id(ENV.fetch("AZURE_ACCESS_TOKEN", nil))
+puts "3"
 puts "user_id #{$user_id}"
+puts "4"
 target_branch_name = $options[:branch] || azure_client1.fetch_default_branch($source.repo)
 active_pull_requests = azure_client1.pull_requests_active_for_user_and_targeting_branch(user_id, target_branch_name)
 
